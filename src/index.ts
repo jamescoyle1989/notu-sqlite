63342c986757d834e909f3b03c6d700811a874bd
James Coyle <jamescoyle1989@gmail.com>
2025-07-18 21:11:18 -0400

    Another attempt to try and get notu-sqlite working with expo

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { Security } from './Security';
 import { NotuSQLiteCacheFetcher } from './NotuSQLiteCacheFetcher';
 import { NotuSQLiteClient } from './NotuSQLiteClient';
 import { BetterSQLiteConnection, ExpoSQLiteConnection } from './SQLiteConnection';
 
 export {
-    Security,
     NotuSQLiteCacheFetcher,
     NotuSQLiteClient,
     BetterSQLiteConnection,
